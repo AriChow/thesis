5475fcd75843c7ec439d36805ae13d03d1267556
Aritra Chowdhury <aritrachowdhury@zaphod.cs.rpi.edu>
2018-05-21 19:14:38 -0400

    Added main contents of defense presentation

--- candidacy/code/error_bars.m
+++ candidacy/code/error_bars.m
@@ -5,8 +5,8 @@ best_configs = {};
 for i = 1 : 5
     for j = 1 : 7
         for k = 1 : 4
-            best_errs = [best_errs, trans_long_results(i, j, k)];
-            best_errs_std = [best_errs_std, trans_long_results_std(i, j, k)];
+            best_errs = [best_errs, matsc_results(i, j, k)];
+            best_errs_std = [best_errs_std, matsc_results_std(i, j, k)];
             best_configs = [best_configs; [{fe{i}}, {fs{j}}, {clf{k}}]];
         end
     end
